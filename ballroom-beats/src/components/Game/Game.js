5b92d2411fca515e27fe44c13b04e4b1637ffc5e
Allison Wagner <allisonjw01@gmail.com>
2020-02-17 15:51:27 -0700

    Adds basic components layout to appNavigator danceType end and game

---
+++ ballroom-beats/src/components/Game/Game.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { withNavigation } from 'react-navigation';
+
+class Game extends Component {
+
+  render() {
+      return (
+          <View style={styles.container}>
+            <Text>Pick Your Song</Text>
+          </View>
+      )      
+  }
+} 
+
+const AppNavigator = createStackNavigator({
+    Game: {
+      screen: Game,
+    },
+});
+  
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
+
+export default withNavigation(Game);
