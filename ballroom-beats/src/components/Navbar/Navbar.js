818eac9caa5d29c2cb2d744bde33d52f4facddbb
Allison Wagner <allisonjw01@gmail.com>
2020-02-18 14:11:27 -0700

    Adds more styling and positions for navbar

--- ballroom-beats/src/components/Navbar/Navbar.js
+++ ballroom-beats/src/components/Navbar/Navbar.js
@@ -1,32 +1,53 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { createAppContainer, withNavigation  } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { withNavigation } from 'react-navigation';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+
 
 class Navbar extends Component {
-
+  
   render() {
-      return (
-          <View style={styles.container}>
-            <Text>Navbar</Text>
-          </View>
-      )      
-  }
-}; 
+    return (
+      <View style={styles.bottomContainer}>
+          <View style={styles.btnContainer}>
+            <Icon.Button 
+                style={styles.button} 
+                name="home" 
+                onPress={() => this.props.navigation.navigate('Home')} 
+                title="BACK">
+                <Text style={styles.home}>Home</Text>
+            </Icon.Button>
+      </View>
+    </View>
+  )
+}
+};
 
 const AppNavigator = createStackNavigator({
     Navbar: {
       screen: Navbar,
     },
 });
-  
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+  bottomContainer: {
+    backgroundColor: 'blue',
+    bottom: 60,
+    width: '100%',
+    position: 'absolute',
+  },
+  btnContainer: {
+    // flex: 1,
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'blue',
+    fontSize: 25,
+  },
+  home: {
+    fontSize: 25,
+  }
 });
 
 export default withNavigation(Navbar);
