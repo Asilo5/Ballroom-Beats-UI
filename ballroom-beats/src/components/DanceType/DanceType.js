11dda6e15d98c41f811653665f16b100e5c01862
Allison Wagner <allisonjw01@gmail.com>
2020-02-17 19:17:38 -0700

    Imports Icon component for images

--- ballroom-beats/src/components/DanceType/DanceType.js
+++ ballroom-beats/src/components/DanceType/DanceType.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { withNavigation } from 'react-navigation';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 class DanceType extends Component {
 
   render() {
       return (
           <View style={styles.container}>
-            <Text>Pick Your Dance Type</Text>
+            <Text style={styles.dance}>Pick Your Dance Type</Text>
           </View>
       )      
   }
@@ -23,10 +24,13 @@ const AppNavigator = createStackNavigator({
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: 'black',
         alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+    dance: {
+
+    },
 });
 
 export default withNavigation(DanceType);
--- ballroom-beats/src/components/Loader/Loader.js
+++ ballroom-beats/src/components/Loader/Loader.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { withNavigation } from 'react-navigation';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 class Loader extends Component {
 
