f35c19622514fe3ad2b43cea50bbb4ef4705a127
Allison Wagner <allisonjw01@gmail.com>
2020-02-17 19:32:44 -0700

    Changes background color to black

--- ballroom-beats/src/components/SongPick/SongPick.js
+++ ballroom-beats/src/components/SongPick/SongPick.js
@@ -38,7 +38,7 @@ const AppNavigator = createStackNavigator({
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: 'black',
         alignItems: 'center',
         justifyContent: 'center',
     },
