e65cd74517e572a190aec1697e7ae241703b9f5d
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-24 13:54:14 -0700

    Add passing test for End component

--- ballroom-beats/src/components/End/End.test.js
+++ ballroom-beats/src/components/End/End.test.js
@@ -2,10 +2,17 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import End from '../End/End';
 
-describe('End', () => {
+const mockProps = {
+   navigation: {
+     navigate: jest.fn()
+   }
+ };
+
+describe('End', () => {  
+
 
    it('should match snapshot', () => {
-    const end = renderer.create(<End />).toJSON();
+    const end = renderer.create(<End navigation={mockProps.navigation} />).toJSON();
     expect(end).toMatchSnapshot();
    });
 
