fd1a39e4c6bdca97c754ba0bf29f123301063e7e
Allison Wagner <allisonjw01@gmail.com>
2020-02-26 20:55:41 -0700

    Adding styling for demo

--- ballroom-beats/src/components/End/End.js
+++ ballroom-beats/src/components/End/End.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { withNavigation } from 'react-navigation';
+import Icon from 'react-native-vector-icons/AntDesign';
+
 
 class End extends Component {
 
@@ -9,12 +11,13 @@ class End extends Component {
       return (
           <View style={styles.container}>
             <Text style={styles.gameEnd}>End Of Game</Text>
-            <Button
+            <Icon.Button
+                name={'playcircleo'}
                 style={styles.button}
                 onPress={() => this.props.navigation.navigate('DanceType')}
                 title="PLAY AGAIN">
                 <Text style={styles.playAgain}>Play Again</Text>
-            </Button>
+            </Icon.Button>
             <Text style={styles.score}>{this.props.navigation.getParam('scoreText', '')}</Text>
           </View>
       )
@@ -45,13 +48,16 @@ const styles = StyleSheet.create({
     score: {
         fontSize: 40,
         color: '#A9C344',
+        textShadowOffset: { width: 2, height: 2 },
+        textShadowRadius: 2,
+        textShadowColor: '#000',
         margin: 10,
         textAlign: "center"
     },
     button: {
-        backgroundColor: '#BD4413',
-        padding: 20,
-        fontSize: 35
+        backgroundColor: '#D1005A',
+        padding: 10,
+        fontSize: 25
     },
     playAgain: {
         fontSize: 30,
